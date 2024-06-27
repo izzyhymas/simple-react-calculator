2fe8a7968de72632f4b6918b2c60dadc42be3e8e
Izzy Hymas <izzyhymas@gmail.com>
2024-06-27 11:06:46 -0600

    Create calculator using react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import './App.css'
+import './App.css';
+import Calculator from './Calculator';
 
 export default function App() {
   return (
     <>
+      <header>
+        <h1>Simple Calculator</h1>
+      </header>
+      <Calculator />
+      <footer>
+        <p>Created by Izzy Hymas</p>
+      </footer>
     </>
   )
 }
